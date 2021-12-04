6cc7f4df511c0a393ed72c9be1e10bbd297c3f5c
Chinedu Ezeigbo <dexterautonomyyahoo.comgit config --global user.email dexterautonomyyahoo.com>
2021-12-04 14:26:16 +0100

    Initial commit 3

--- src/main/java/com/recruiter/devops/controller/UserController.java
+++ src/main/java/com/recruiter/devops/controller/UserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,6 +19,7 @@ public class UserController {
 	@Autowired
 	private UserServiceImpl userServiceImpl;
 	
+	@GetMapping("fetUser")
 	public List<UserDTO> fetchUser(){
 		return userServiceImpl.fetUsers().get();
 	}
